460cae0edb66c99f11ef0ef8b685ce051b4151b9
rbalexan <rbalexan@stanford.edu>
2021-08-06 19:17:42 -0700

    add homework 4

---
+++ homework/homework-4/code/data/data.R
@@ -0,0 +1,14 @@
+setwd("/Users/tranlm/Google Drive/homework/hw4/data")
+
+# Ch8 Q8
+data(Carseats)
+write.csv(Carseats, file='./Carseats.csv', row.names=FALSE)
+
+# Ch8 Q10
+data(Hitters)
+write.csv(Hitters, file='./Hitters.csv', row.names=TRUE)
+
+# Ch8 Q11
+data(Caravan)
+write.csv(Caravan, file='./Caravan.csv', row.names=FALSE)
+
